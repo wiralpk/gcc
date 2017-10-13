9aadb1b636b07a8c0ef87e78cdf18659b6c11487
Richard Sandiford <richard.sandiford@linaro.org>
2017-10-13 10:49:30 +0100

    poly_int: brig vector elements
    This patch adds a brig-specific wrapper around TYPE_VECTOR_SUBPARTS,
    since presumably it will never need to support variable vector lengths.
    
    2017-10-17  Richard Sandiford  <richard.sandiford@linaro.org>
    	    Alan Hayward  <alan.hayward@arm.com>
    	    David Sherwood  <david.sherwood@arm.com>
    
    gcc/brig/
    	* brigfrontend/brig-util.h (gccbrig_type_vector_subparts): New
    	function.
    	* brigfrontend/brig-basic-inst-handler.cc
    	(brig_basic_inst_handler::build_shuffle): Use it instead of
    	TYPE_VECTOR_SUBPARTS.
    	(brig_basic_inst_handler::build_unpack): Likewise.
    	(brig_basic_inst_handler::build_pack): Likewise.
    	(brig_basic_inst_handler::build_unpack_lo_or_hi): Likewise.
    	(brig_basic_inst_handler::operator ()): Likewise.
    	(brig_basic_inst_handler::build_lower_element_broadcast): Likewise.
    	* brigfrontend/brig-code-entry-handler.cc
    	(brig_code_entry_handler::get_tree_cst_for_hsa_operand): Likewise.
    	(brig_code_entry_handler::get_comparison_result_type): Likewise.
    	(brig_code_entry_handler::expand_or_call_builtin): Likewise.

--- gcc/brig/brigfrontend/brig-util.h
+++ gcc/brig/brigfrontend/brig-util.h
@@ -76,4 +76,12 @@ bool gccbrig_might_be_host_defined_var_p (const BrigDirectiveVariable *brigVar);
 /* From hsa.h.  */
 bool hsa_type_packed_p (BrigType16_t type);
 
+/* Return the number of elements in a VECTOR_TYPE.  BRIG does not support
+   variable-length vectors.  */
+inline unsigned HOST_WIDE_INT
+gccbrig_type_vector_subparts (const_tree type)
+{
+  return TYPE_VECTOR_SUBPARTS (type);
+}
+
 #endif
